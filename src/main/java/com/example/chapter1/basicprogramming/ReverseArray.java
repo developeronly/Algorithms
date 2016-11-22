4ae5fe9f663b76714257d0cb1bda81a9a297b670
developeronly <developeronly@gmail.com>
2016-11-22 21:42:49 +0530

    Reverse an array: Implementing other implementations.

--- src/main/java/com/example/chapter1/basicprogramming/ReverseArray.java
+++ src/main/java/com/example/chapter1/basicprogramming/ReverseArray.java
@@ -1,5 +1,10 @@
 package com.example.chapter1.basicprogramming;
 
+import com.example.chapter1.basicprogramming.models.Student;
+
+//TODO: I'm not in favor of static methods. If someone want's make it better please contribute.
+//TODO: I'm not able to make methods generic which accepts primitives. If someone make it better please contribute.
+//TODO: Logic seems to duplicated. Refactor it!!
 public class ReverseArray {
 
 
@@ -12,12 +17,35 @@ public class ReverseArray {
         }
         return numericArray;
     }
-}
 
-/*
-    TODO:
-    can we make it generic?
-    public static <T> T[] reverse(T[] array) {
-        return array;
+    public static double[] reverse(double[] numericArray) {
+        int arrayLength = numericArray.length;
+        for (int index = 0; index < arrayLength / 2; index++) {
+            double temp = numericArray[index];
+            numericArray[index] = numericArray[arrayLength - 1 - index];
+            numericArray[arrayLength - index - 1] = temp;
+        }
+        return numericArray;
     }
-*/
+
+
+    public static String[] reverse(String[] stringArray) {
+        int arrayLength = stringArray.length;
+        for (int index = 0; index < arrayLength / 2; index++) {
+            String temp = stringArray[index];
+            stringArray[index] = stringArray[arrayLength - 1 - index];
+            stringArray[arrayLength - index - 1] = temp;
+        }
+        return stringArray;
+    }
+
+    public static Student[] reverse(Student[] studentArray) {
+        int arrayLength = studentArray.length;
+        for (int index = 0; index < arrayLength / 2; index++) {
+            Student temp = studentArray[index];
+            studentArray[index] = studentArray[arrayLength - 1 - index];
+            studentArray[arrayLength - index - 1] = temp;
+        }
+        return studentArray;
+    }
+}
--- src/test/java/com/example/chapter1/basicprogramming/ReverseArrayTest.java
+++ src/test/java/com/example/chapter1/basicprogramming/ReverseArrayTest.java
@@ -1,5 +1,6 @@
 package com.example.chapter1.basicprogramming;
 
+import com.example.chapter1.basicprogramming.models.Student;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -12,21 +13,37 @@ public class ReverseArrayTest {
         int[] expectedNumericArray = new int[]{21, 23, 4, 2};
         assertArrayEquals("Testing reversing numeric array...",
                 ReverseArray.reverse(inputNumericArray), expectedNumericArray);
+    }
 
+    @Test
+    public void reverseFloatArray() {
+        double[] inputNumericArray = new double[]{2.3, 4.23, 23.33, 21.55};
+        double[] expectedNumericArray = new double[]{21.55, 23.33, 4.23, 2.3};
+        assertArrayEquals("Testing reversing float array...",
+                ReverseArray.reverse(inputNumericArray), expectedNumericArray, 0.001);
 
     }
 
+    @Test
+    public void reverseStringArray() {
+        String[] inputStringArray = new String[]{"A", "v", "b", "F", "k"};
+        String[] expectedStringArray = new String[]{"k", "F", "b", "v", "A"};
+        assertArrayEquals("Testing reversing string array...",
+                ReverseArray.reverse(inputStringArray), expectedStringArray);
+    }
+
+    @Test
+    public void reverseObjectArray() {
+        Student[] inputStudentArray = new Student[3];
+        inputStudentArray[0] = new Student(1, "A", "asd");
+        inputStudentArray[1] = new Student(2, "B", "zxc");
+        inputStudentArray[2] = new Student(3, "C", "qwe");
+        Student[] expectedStudentArray = new Student[3];
+        expectedStudentArray[2] = new Student(1, "A", "asd");
+        expectedStudentArray[1] = new Student(2, "B", "zxc");
+        expectedStudentArray[0] = new Student(3, "C", "qwe");
+        assertArrayEquals("Testing object array...",
+                ReverseArray.reverse(inputStudentArray), expectedStudentArray);
+    }
+
 }
-
-/*
-TODO:
-reverse
-numeric array
-float array
-string array
-empty array
-large array
-odd length
-even length
-*/
-
