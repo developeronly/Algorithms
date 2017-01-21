928b01dc17aba7f727e0e10a217a348110ce60a4
developeronly <developeronly@gmail.com>
2017-01-21 17:53:56 +0530

    Make union find methods package private.

--- src/main/java/com/example/chapter1/unionfind/Union.java
+++ src/main/java/com/example/chapter1/unionfind/Union.java
@@ -5,45 +5,45 @@ public abstract class Union {
     private int[] ids;
     private int numberOfUnionFound;
 
-    public Union(int numberOfSites) {
+    protected Union(int numberOfSites) {
         this.numberOfUnionFound = numberOfSites;
         this.ids = new int[numberOfSites];
         for (int index = 0; index < numberOfSites; index++)
             ids[index] = index;
     }
 
-    public abstract int find(int site);
+    protected abstract int find(int site);
 
-    public abstract void union(int firstSite, int secondSite);
+    protected abstract void union(int firstSite, int secondSite);
 
-    public abstract void printUnions();
+    protected abstract void printUnions();
 
-    public boolean isConnected(int firstSite, int secondSite) throws ArrayIndexOutOfBoundsException {
+    protected boolean isConnected(int firstSite, int secondSite) throws ArrayIndexOutOfBoundsException {
         return find(firstSite) == find(secondSite);
     }
 
-    public void addSite(int firstSite, int secondSite) throws ArrayIndexOutOfBoundsException {
+    protected void addSite(int firstSite, int secondSite) throws ArrayIndexOutOfBoundsException {
         if (isConnected(firstSite, secondSite)) return;
         union(firstSite, secondSite);
     }
 
-    public int count() {
+    protected int count() {
         return numberOfUnionFound;
     }
 
-    public int[] getIds() {
+    protected int[] getIds() {
         return ids;
     }
 
-    public void setIds(int[] ids) {
+    protected void setIds(int[] ids) {
         this.ids = ids;
     }
 
-    public int getNumberOfUnionFound() {
+    protected int getNumberOfUnionFound() {
         return numberOfUnionFound;
     }
 
-    public void setNumberOfUnionFound(int numberOfUnionFound) {
+    protected void setNumberOfUnionFound(int numberOfUnionFound) {
         this.numberOfUnionFound = numberOfUnionFound;
     }
 
