ca1ba46b223cda6bbf701496f419d547144cf2ee
developeronly <developeronly@gmail.com>
2017-03-02 21:29:51 +0530

    added implementation for select, rank, deleteMin, deleteMax, delete in BinarySearchTree.

--- src/main/java/com/example/chapter3/binarysearchtrees/BinarySearchTree.java
+++ src/main/java/com/example/chapter3/binarysearchtrees/BinarySearchTree.java
@@ -13,6 +13,10 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         else return root.size;
     }
 
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
     public void put(Key key, Value value) {
         root = put(root, key, value);
     }
@@ -92,6 +96,73 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
             else return root;
         }
         return ceiling(root.right, key);
+    }
+
+    public Key select(int index) {
+        return select(root, index).key;
+    }
+
+    private Node select(Node root, int index) {
+        if (root == null) return null;
+        int size = size(root.left);
+        if (size > index) return select(root.left, index);
+        else if (size < index) return select(root.right, index - size - 1);
+        else return root;
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node root) {
+        if (root == null) return 0;
+        int compare = key.compareTo(root.key);
+        if (compare < 0) return rank(key, root.left);
+        else if (compare > 0) return 1 + size(root.left) + rank(key, root.right);
+        else return size(root.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node root) {
+        if (root.left == null) return root.right;
+        root.left = deleteMin(root.left);
+        root.size = size(root.left) + size(root.right) + 1;
+        return root;
+    }
+
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node root) {
+        if (root.right == null) return root.left;
+        root.right = deleteMax(root.right);
+        root.size = size(root.right) + size(root.left) + 1;
+        return root;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node root, Key key) {
+        if (root == null) return null;
+        int compare = key.compareTo(root.key);
+        if (compare < 0) root.left = delete(root.left, key);
+        else if (compare > 0) root.right = delete(root.right, key);
+        else {
+            if (root.right == null) return root.left;
+            if (root.left == null) return root.right;
+            Node node = root;
+            root = min(node.right);
+            root.right = deleteMin(node.right);
+            root.left = node.left;
+        }
+        root.size = size(root.right) + size(root.left) + 1;
+        return root;
     }
 
     private class Node {
--- src/test/java/com/example/chapter3/binarysearchtrees/BinarySearchTreeTest.java
+++ src/test/java/com/example/chapter3/binarysearchtrees/BinarySearchTreeTest.java
@@ -4,6 +4,7 @@ import com.example.models.Student;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class BinarySearchTreeTest {
@@ -12,6 +13,7 @@ public class BinarySearchTreeTest {
     public void init() {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         assertEquals(0, binarySearchTree.size());
+        assertTrue(binarySearchTree.isEmpty());
     }
 
     public BinarySearchTree constructDummyTree() {
@@ -20,6 +22,24 @@ public class BinarySearchTreeTest {
         binarySearchTree.put(4, "Raj");
         binarySearchTree.put(2, "Mahesh");
         binarySearchTree.put(1, "Akshay");
+        return binarySearchTree;
+    }
+
+    public BinarySearchTree constructDummyStringTree() {
+        BinarySearchTree<String, String> binarySearchTree = new BinarySearchTree();
+        binarySearchTree.put("s", "S");
+        binarySearchTree.put("e", "E");
+        binarySearchTree.put("a", "A");
+        binarySearchTree.put("r", "R");
+        binarySearchTree.put("c", "C");
+        binarySearchTree.put("h", "H");
+        binarySearchTree.put("e", "E");
+        binarySearchTree.put("x", "X");
+        binarySearchTree.put("a", "A");
+        binarySearchTree.put("m", "M");
+        binarySearchTree.put("p", "P");
+        binarySearchTree.put("l", "L");
+        binarySearchTree.put("e", "E");
         return binarySearchTree;
     }
 
@@ -91,5 +111,41 @@ public class BinarySearchTreeTest {
         assertEquals(4, binarySearchTree.ceiling(5).longValue());
         assertEquals(null, binarySearchTree.ceiling(0));
     }
+
+    @Test
+    public void selectKey() {
+        BinarySearchTree<Integer, String> binarySearchTree = constructDummyTree();
+        assertEquals(4, binarySearchTree.select(3).longValue());
+    }
+
+    @Test
+    public void rankKey() {
+        BinarySearchTree<Integer, String> binarySearchTree = constructDummyTree();
+        assertEquals(2, binarySearchTree.rank(3));
+    }
+
+    @Test
+    public void deleteMin() {
+        BinarySearchTree<Integer, String> binarySearchTree = constructDummyStringTree();
+        assertEquals("a", binarySearchTree.min());
+        binarySearchTree.deleteMin();
+        assertEquals("c", binarySearchTree.min());
+    }
+
+    @Test
+    public void deleteMax() {
+        BinarySearchTree<String, String> binarySearchTree = constructDummyStringTree();
+        assertEquals("x", binarySearchTree.max());
+        binarySearchTree.deleteMax();
+        assertEquals("s", binarySearchTree.max());
+    }
+
+    @Test
+    public void delete() {
+        BinarySearchTree<String, String> binarySearchTree = constructDummyStringTree();
+        binarySearchTree.delete("e");
+        assertEquals(null, binarySearchTree.get("e"));
+    }
+
 
 }
