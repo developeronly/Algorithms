d3c2e0ef36c8119ce7ad4680101691ffab861a87
developeronly <developeronly@gmail.com>
2017-03-06 22:42:19 +0530

    Added implementation of balanced binary trees. RedBlackBinarySearchTree.

---
+++ src/test/java/com/example/chapter3/balancedsearchtrees/RedBlackBinarySearchTreeTest.java
@@ -0,0 +1,167 @@
+package com.example.chapter3.balancedsearchtrees;
+
+import com.example.models.Student;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RedBlackBinarySearchTreeTest {
+
+    public RedBlackBinarySearchTree constructDummyStringTree() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = new RedBlackBinarySearchTree();
+        redBlackBinarySearchTree.put("s", "S");
+        redBlackBinarySearchTree.put("e", "E");
+        redBlackBinarySearchTree.put("a", "A");
+        redBlackBinarySearchTree.put("r", "R");
+        redBlackBinarySearchTree.put("c", "C");
+        redBlackBinarySearchTree.put("h", "H");
+        redBlackBinarySearchTree.put("e", "E");
+        redBlackBinarySearchTree.put("x", "X");
+        redBlackBinarySearchTree.put("a", "A");
+        redBlackBinarySearchTree.put("m", "M");
+        redBlackBinarySearchTree.put("p", "P");
+        redBlackBinarySearchTree.put("l", "L");
+        redBlackBinarySearchTree.put("e", "E");
+        return redBlackBinarySearchTree;
+    }
+
+    @Test
+    public void init() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = new RedBlackBinarySearchTree();
+        assertEquals(0, redBlackBinarySearchTree.size());
+        assertTrue(redBlackBinarySearchTree.isEmpty());
+        redBlackBinarySearchTree = constructDummyStringTree();
+        assertFalse(redBlackBinarySearchTree.isEmpty());
+    }
+
+    @Test
+    public void putString() {
+        RedBlackBinarySearchTree<Integer, String> redBlackBinarySearchTree = new RedBlackBinarySearchTree();
+        redBlackBinarySearchTree.put(0, "Suraj");
+        assertEquals(1, redBlackBinarySearchTree.size());
+        redBlackBinarySearchTree.put(1, "Raj");
+        assertEquals(2, redBlackBinarySearchTree.size());
+        redBlackBinarySearchTree.put(2, "Mahesh");
+        assertEquals(3, redBlackBinarySearchTree.size());
+    }
+
+    @Test
+    public void getString() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("S", redBlackBinarySearchTree.get("s"));
+        assertEquals("X", redBlackBinarySearchTree.get("x"));
+        assertEquals(null, redBlackBinarySearchTree.get("Unexpected"));
+    }
+
+    @Test
+    public void putAndGetForGeneric() {
+        RedBlackBinarySearchTree<Student, String> redBlackBinarySearchTree = new RedBlackBinarySearchTree<>();
+        Student firstStudent = new Student(1, "Suraj", "Pune");
+        redBlackBinarySearchTree.put(firstStudent, "Suraj");
+        assertEquals(redBlackBinarySearchTree.get(firstStudent), "Suraj");
+        Student secondStudent = new Student(2, "Raj", "Satara");
+        redBlackBinarySearchTree.put(secondStudent, "Raj");
+        assertEquals(redBlackBinarySearchTree.get(secondStudent), "Raj");
+        Student thirdStudent = new Student(3, "Mahesh", "Mumbai");
+        redBlackBinarySearchTree.put(thirdStudent, "Mahesh");
+        assertEquals(redBlackBinarySearchTree.get(thirdStudent), "Mahesh");
+        Student fourthStudent = new Student(3, "Akshay", "Karad");
+        redBlackBinarySearchTree.put(fourthStudent, "Akshay");
+        assertEquals(redBlackBinarySearchTree.get(fourthStudent), "Akshay");
+    }
+
+    @Test
+    public void minKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("a", redBlackBinarySearchTree.min());
+    }
+
+    @Test
+    public void maxKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("x", redBlackBinarySearchTree.max());
+    }
+
+    @Test
+    public void floorKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("x", redBlackBinarySearchTree.floor("x"));
+        assertEquals("a", redBlackBinarySearchTree.floor("a"));
+        assertEquals("x", redBlackBinarySearchTree.floor("z"));
+        assertEquals("m", redBlackBinarySearchTree.floor("o"));
+    }
+
+    @Test
+    public void ceilingKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("a", redBlackBinarySearchTree.ceiling("a"));
+        assertEquals("x", redBlackBinarySearchTree.ceiling("x"));
+        assertEquals("p", redBlackBinarySearchTree.ceiling("o"));
+        assertEquals(null, redBlackBinarySearchTree.ceiling("z"));
+    }
+
+    @Test
+    public void selectKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("a", redBlackBinarySearchTree.select(0));
+        assertEquals("c", redBlackBinarySearchTree.select(1));
+        assertEquals("e", redBlackBinarySearchTree.select(2));
+    }
+
+    @Test
+    public void rankKey() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals(0, redBlackBinarySearchTree.rank("a"));
+        assertEquals(8, redBlackBinarySearchTree.rank("s"));
+    }
+
+    @Test
+    public void deleteMin() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("a", redBlackBinarySearchTree.min());
+        redBlackBinarySearchTree.deleteMin();
+        assertEquals("c", redBlackBinarySearchTree.min());
+    }
+
+    @Test
+    public void deleteMax() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertEquals("x", redBlackBinarySearchTree.max());
+        redBlackBinarySearchTree.deleteMax();
+        assertEquals("s", redBlackBinarySearchTree.max());
+    }
+
+    @Test
+    public void contains() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        assertTrue(redBlackBinarySearchTree.contains("a"));
+        assertFalse(redBlackBinarySearchTree.contains("z"));
+    }
+
+    @Test
+    public void delete() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        redBlackBinarySearchTree.delete("e");
+        assertEquals(null, redBlackBinarySearchTree.get("e"));
+    }
+
+    @Test
+    public void print() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        redBlackBinarySearchTree.print();
+        System.out.println();
+    }
+
+    @Test
+    public void keys() {
+        RedBlackBinarySearchTree<String, String> redBlackBinarySearchTree = constructDummyStringTree();
+        System.out.println();
+        redBlackBinarySearchTree.keys().forEach(key -> System.out.print(" " + key));
+        System.out.println();
+        redBlackBinarySearchTree.keys("f", "t").forEach(key -> System.out.print(" " + key));
+        System.out.println();
+    }
+
+}
